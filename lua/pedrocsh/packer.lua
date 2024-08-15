be35e4aabc878ae71e77cfad1636c8e93c86ad66
Pedro Cardoso <peedro.shell@gmail.com>
2024-08-14 22:51:41 -0300

    config undotree

--- lua/pedrocsh/packer.lua
+++ lua/pedrocsh/packer.lua
@@ -7,31 +7,37 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- undotree
+  use 'mbbill/undotree'
+
+  -- theme
   use { "catppuccin/nvim", as = "catppuccin" }
+
   -- telescope
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
   -- treesitter
-  use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
+  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
   use {
-  "nvim-neo-tree/neo-tree.nvim",
+    "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
-    requires = { 
+    requires = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     }
   }
+
   -- lsp-zero
-  use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
-  use({'neovim/nvim-lspconfig'})
-  use({'hrsh7th/nvim-cmp'})
-  use({'hrsh7th/cmp-nvim-lsp'})
+  use({ 'VonHeikemen/lsp-zero.nvim', branch = 'v4.x' })
+  use({ 'neovim/nvim-lspconfig' })
+  use({ 'hrsh7th/nvim-cmp' })
+  use({ 'hrsh7th/cmp-nvim-lsp' })
 
   -- mason
   use {
