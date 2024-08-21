e4a8408fcea77f703d27cb4bcba1bc6bdf1a8fd4
Pedro Cardoso <peedro.shell@gmail.com>
2024-08-21 01:40:41 -0300

    remove ts-comment

--- lua/pedrocsh/packer.lua
+++ lua/pedrocsh/packer.lua
@@ -19,9 +19,6 @@ return require('packer').startup(function(use)
   -- formatter
   use 'stevearc/conform.nvim'
 
-  -- ts-comment
-  use 'folke/ts-comments.nvim'
-
   -- telescope
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
