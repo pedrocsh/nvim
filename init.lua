6aacc5d59bf8519f418cbb998b626671eaaf6dea
Pedro Cardoso <peedro.shell@gmail.com>
2024-08-25 23:48:56 -0300

    setup lualine

--- init.lua
+++ init.lua
@@ -128,7 +128,33 @@ require("lazy").setup({
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {},
+		opts = {
+			theme = "auto",
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff" },
+				lualine_c = { "buffers" },
+				lualine_x = { "tabs" },
+				lualine_y = { "progress" },
+				lualine_z = {
+					{
+						"diagnostics",
+						sources = { "nvim_diagnostic", "nvim_lsp" },
+						sections = { "error", "warn", "info", "hint" },
+						diagnostics_color = {
+							error = "DiagnosticError",
+							warn = "DiagnosticWarn",
+							info = "DiagnosticInfo",
+							hint = "DiagnosticHint",
+						},
+						symbols = { error = "E", warn = "W", info = "I", hint = "H" },
+						colored = true,
+						update_in_insert = false,
+						always_visible = false,
+					},
+				},
+			},
+		},
 	},
 	{
 		"folke/lazydev.nvim",
@@ -267,6 +293,7 @@ require("lazy").setup({
 				lua = { "stylua" },
 				python = { "isort", "black", stop_after_first = true },
 				javascript = { "eslint-lsp", "eslint-d", stop_after_first = true },
+				prisma = { "prisma-language-server" },
 			},
 		},
 	},
