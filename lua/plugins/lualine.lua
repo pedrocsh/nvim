90cb45b28bced0b46d0cf874373cefa4f4b0f518
Pedro Cardoso <peedro.shell@gmail.com>
2024-08-26 16:21:03 -0300

    install lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = {
+            sections = {
+                lualine_a = {"mode"},
+                lualine_b = {"branch", "diff"},
+                lualine_c = {"buffers"},
+                lualine_x = {"tabs", "filetype"},
+                lualine_y = {"progress"},
+                lualine_z = {
+                    { "diagnostics",
+                        sources = {"nvim_diagnostic", "nvim_lsp"},
+                        sections = {"error", "warn", "info", "hint"},
+                        diagnostics_color = {
+                            error = "DiagnosticError",
+                            warn  = "DiagnosticWarn",
+                            info  = "DiagnosticInfo",
+                            hint  = "DiagnosticHint",
+                        },
+                        symbols = {error = "E", warn = "W", info = "I", hint = "H"},
+                        colored = true,
+                        update_in_insert = false,
+                        always_visible = false,
+                    }
+                }
+            }
+        },
+        dependencies = { "nvim-tree/nvim-web-devicons" }
+    }
+}
