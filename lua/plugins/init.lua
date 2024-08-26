fa4f7accee40af81adca6aecabced74f52891828
Pedro Cardoso <peedro.shell@gmail.com>
2024-08-26 17:11:23 -0300

    install lazydev

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,4 +4,13 @@ return {
         dependencies = { "nvim-lua/plenary.nvim" },
         opts = { signs = false }
     },
+    {
+        'folke/lazydev.nvim',
+        ft = 'lua',
+        opts = {
+            library = {
+                { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+            },
+        },
+    },
 }
