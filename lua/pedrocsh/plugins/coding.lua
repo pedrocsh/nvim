d9bc5bf2772c4cc92c5f4760a624cc102649599e
Pedro Cardoso <peedro.shell@gmail.com>
2024-08-31 00:49:35 -0300

    add telescope-file-browser

--- lua/pedrocsh/plugins/coding.lua
+++ lua/pedrocsh/plugins/coding.lua
@@ -12,9 +12,9 @@ return {
         build = "make install_jsregexp",
         dependencies = {
           {
-            'rafamadriz/friendly-snippets',
+            "rafamadriz/friendly-snippets",
             config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
+              require("luasnip.loaders.from_vscode").lazy_load()
             end,
           },
         },
@@ -34,37 +34,37 @@ return {
           end
         },
         completion = {
-          completeopt = 'menu,menuone,noinsert'
+          completeopt = "menu,menuone,noinsert"
         },
         mapping = cmp.mapping.preset.insert {
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
-          ['<C-Space>'] = cmp.mapping.complete {},
-          ['<C-l>'] = cmp.mapping(function()
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-y>"] = cmp.mapping.confirm { select = true },
+          ["<C-Space>"] = cmp.mapping.complete {},
+          ["<C-l>"] = cmp.mapping(function()
             if luasnip.expand_or_locally_jumpable() then
               luasnip.expand_or_jump()
             end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
+          end, { "i", "s" }),
+          ["<C-h>"] = cmp.mapping(function()
             if luasnip.locally_jumpable(-1) then
               luasnip.jump(-1)
             end
-          end, { 'i', 's' }),
+          end, { "i", "s" }),
         },
         sources = {
           {
-            name = 'lazydev',
+            name = "lazydev",
             group_index = 0,
           },
           { name = "nvim_lsp" },
-          { name = 'luasnip' },
-          { name = 'path' },
+          { name = "luasnip" },
+          { name = "path" },
         },
         {
-          { name = 'buffer' }
+          { name = "buffer" }
         },
       }
     end
@@ -106,55 +106,63 @@ return {
     }
   },
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    "nvim-telescope/telescope.nvim", tag = "0.1.8",
     dependencies = {
-      'nvim-lua/plenary.nvim',
+      "nvim-lua/plenary.nvim",
       {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = {
+          "nvim-telescope/telescope.nvim",
+          "nvim-lua/plenary.nvim"
+        }
+      },
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
         cond = function()
-          return vim.fn.executable 'make' == 1
+          return vim.fn.executable "make" == 1
         end,
       },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font }
+      { "nvim-telescope/telescope-ui-select.nvim" },
+      { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font }
     },
     opts = function ()
       local builtin = require("telescope.builtin")
       local map = vim.keymap.set
 
-      pcall(require('telescope').load_extension, 'fzf')
-      pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require("telescope").load_extension, "fzf")
+      pcall(require("telescope").load_extension, "ui-select")
+      pcall(require("telescope").load_extension, "file_browser")
 
-      map('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      map('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      map('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      map('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      map('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      map('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      map('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      map('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      map('n', '<leader>gf', builtin.git_files, { desc = '[G]it [F]iles' })
-      map('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      map('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      map("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+      map("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+      map("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+      map("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+      map("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+      map("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+      map("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+      map("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+      map("n", "<leader>gf", builtin.git_files, { desc = "[G]it [F]iles" })
+      map("n", "<leader>s.", builtin.oldfiles, { desc = "[S]earch Recent Files ('.' for repeat)" })
+      map("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
-      map('n', '<leader>/', function()
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+      map("n", "<leader>/", function()
+        builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
           winblend = 10,
           previewer = false,
         })
-      end, { desc = '[/] Fuzzily search in current buffer' })
+      end, { desc = "[/] Fuzzily search in current buffer" })
 
-      map('n', '<leader>s/', function()
+      map("n", "<leader>s/", function()
         builtin.live_grep {
           grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
+          prompt_title = "Live Grep in Open Files",
         }
-      end, { desc = '[S]earch [/] in Open Files' })
+      end, { desc = "[S]earch [/] in Open Files" })
 
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
+      vim.keymap.set("n", "<leader>sn", function()
+        builtin.find_files { cwd = vim.fn.stdpath "config" }
+      end, { desc = "[S]earch [N]eovim files" })
 
       return {
         defaults = {
@@ -162,8 +170,8 @@ return {
           selection_caret = "# "
         },
         extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown(),
           }
         }
       }
