4c1816bfa7a93ce99c0f4e2146f07117492ac8dd
Pedro Cardoso <peedro.shell@gmail.com>
2024-08-31 00:36:11 -0300

    add some cool maps to telescope

--- lua/pedrocsh/plugins/lsp.lua
+++ lua/pedrocsh/plugins/lsp.lua
@@ -28,6 +28,26 @@ return {
         }
       })
 
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('me-lsp-attach', { clear = true }),
+        callback = function(event)
+          local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          end
+
+          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        end,
+      })
+
       local capabilities = vim.lsp.protocol.make_client_capabilities()
 
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
@@ -45,6 +65,9 @@ return {
             capabilities = capabilities,
             settings = {
               Lua = {
+                completion = {
+                  callSnippet = 'Replace',
+                },
                 diagnostics = {
                   globals = { "vim" }
                 }
